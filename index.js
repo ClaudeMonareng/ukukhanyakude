c5d8124b731beb1b35e698f69872f1f01ff071c2
Lehlohonolo Monareng <lehlohonolomonareng@yahoo.com>
2024-08-14 06:40:58 +0200

    Latest Changes

--- index.js
+++ index.js
@@ -1,46 +1,73 @@
-        // TOGGLE NAVBAR
-        
-        let menu = document.querySelector('#menu-bars');
-        let navbar = document.querySelector('.navbar');
 
-        menu.addEventListener('click', ()=>{
-            menu.classList.toggle('fa-times');
-            navbar.classList.toggle('nav-toggle');
-        });
+// RESPONSIVE NAVIGATION MENU TOGGLE
 
-        window.onscroll = ()=>{
-            menu.classList.remove('fa-times');
-            navbar.classList.remove('nav-toggle');
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
+
+// STICKY NAVBAR                             
+// NAVIGATION BAR EFFECTS ON SCROLL
+
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector("header");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    });
+
+// NAVIGATION MENU ITEMS ACTIVE ON PAGE SCROLL
+
+window.addEventListener("scroll", ()=>{
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        }else{
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
         }
+    });
+});
 
-        // STICKY NAVBAR                            
-        // NAVIGATION BAR EFFECTS ON SCROLL 
 
-        window.addEventListener("scroll", function () {
-            const header = document.querySelector("header");
-            header.classList.toggle("sticky", window.scrollY > 0);
+// PORTFOLIO SECTION - MODAL
+
+const portfolioModals = document.querySelectorAll(".portfolio-modal");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
         });
-
-        // PORTFOLIO SECTION - MODAL
-
-        const portfolioModals = document.querySelectorAll(".portfolio-modal");
-        const imgCards = document.querySelectorAll(".img-card");
-        const portfolioCloseBtns= document.querySelectorAll(".portfolio-close-btn");
-
-        var portfolioModal = function(modalClick){
-            portfolioModals[modalClick].classList.add("active");
-        }
-        
-        imgCards.forEach((imgCard, i) => {
-            imgCard.addEventListener("click", () => {
-                portfolioModal(i);
-            });
-        });
-
-        portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-            portfolioCloseBtn.addEventListener("click", () => {
-            portfolioModals.forEach((portfolioModalView) => {
-                    portfolioModalView.classList.remove("active");
-                });
-            });
-        });
+    });
+});
